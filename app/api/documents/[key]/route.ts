b87479c46cea4d6e79dfd8f1c1f6dd32a27d3b75
kotaro1574 <shake1574@gmail.com>
2024-03-09 09:39:14 -0800

    fix: endpoint

--- app/api/documents/[key]/route.ts
+++ app/api/documents/[key]/route.ts
@@ -1,4 +1,3 @@
-// app/api/documents/[key]/route.ts
 import { NextResponse } from "next/server"
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
